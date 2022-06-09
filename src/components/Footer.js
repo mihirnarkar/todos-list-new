db2e689aa6363fd3cb71e0e39b4753623232f6b9
Mihir Narkar <Mihirnarkar19@gmail.com>
2022-06-09 21:07:26 +0530

    done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,15 @@ import React from 'react'
 export const Footer = () => {
   return (
     <div>
-        Footer works!
+      <div className="card text-center">
+        <div className="card-body">
+          <h5 className="card-title">TODOS | codewithmixxz | All rights reserverd | 2022</h5>
+          <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        </div>
+        <div className="card-footer text-muted">
+          2 days ago
+        </div>
+      </div>
     </div>
   )
 }
